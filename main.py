ba88b633fa5a3a933610a0084dd0562f821f9a37
leo <leonardo.vargas29@gmail.com>
2024-07-09 10:52:09 -0300

    added comments for clarity

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from cases import IEEE30Bus, Saadat3Bus, IEEE14Bus, Class4Bus, Switch4Bus, Class
 # ----------------------- CONFIG ---------------------------
 np.set_printoptions(precision=4)
 stopCondition = 0.001
-maxIter = 10
+maxIter = 100
 baseMVA = 100
 
 
 print('------------- Load case Data -----------------')
 
-
-caseData = Class5BusOpened()
+#para modificar os casos basta mudar aqui
+caseData = Class5Bus()
 print(np.absolute(caseData['Ybus']))
 print(np.angle(caseData['Ybus'], deg=True))
 print(f'is Ybus symmetric? {np.allclose(caseData["Ybus"], caseData["Ybus"].T, rtol=1e-05, atol=1e-08)}')
@@ -22,10 +22,10 @@ print('')
 
 print('-------------- Opening Switches --------------')
 
-#caseData = openSwitches(caseData, switches=[])
+#caseData = openSwitches(caseData, switches=[1, 2, 3...])
 print(caseData['Ybus'])
 
-
+#se quiser resolver o fluxo continuado basta mudar a função solveNRPF para solveCNRPF
 print('')
 print('--------------- Calculations ---------------')
 if caseData['Ybus'].shape[0] > 1:
